809de3293efd26eac8bcac862225ec465cebe872
j1nma <j1nma@users.noreply.github.com>
2018-03-28 13:59:31 -0300

    Works! If run from macOS, sed command appends /n to end. Unfortunate.

--- ejIPC-4.c
+++ ejIPC-4.c
@@ -7,92 +7,145 @@
 #include <errno.h>
 #include <sys/select.h>
 #include <assert.h>
-
-//JUAN
 #include "buffer.h"
 
-static int pipeToChild[2];
-static int pipeFromChild[2];
+
+static int pipeToChild[2] = {-1, -1};
+static int pipeFromChild[2] = {-1, -1};
+static int stdinFD = STDIN_FILENO;
+static int stdoutFD = STDOUT_FILENO;
 
 static unsigned char inParity = 0, outParity = 0;
 
-static int calcParity(struct buffer * buff) {
+static int calcParity(uint8_t * ptr, ssize_t size) {
     unsigned char b = 0;
     int i;
-    size_t * read_bytes = 0;
-    uint8_t * ptr;
 
-    ptr = buffer_read_ptr( buff, read_bytes );
-
-    for ( i = 0 ; i < *read_bytes ; i++ ) {
+    for ( i = 0 ; i < size ; i++ ) {
         b ^= ptr[i] ;    
     }
 
     return b;
 }
 
-static int handleReadStdIn(struct buffer * buff) {
+static void handleReadStdIn(struct buffer * buff) {
 
-    int bytesRead = 0;
+    ssize_t bytesRead = 0;
 
-    size_t * bytes = 0;
+    size_t write_bytes[1] = {0};
 
-    bytesRead = read(STDIN_FILENO, buffer_write_ptr(buff, bytes), *bytes);
+    uint8_t * ptr = buffer_write_ptr(buff, write_bytes);
+
+    bytesRead = read(stdinFD, ptr, *write_bytes);
 
     if (bytesRead > 0) {
 
-        inParity ^= calcParity(buff);
+        inParity ^= calcParity(ptr, bytesRead);
+
+        buffer_write_adv(buff, bytesRead);
+
+        printf("Read from stdin: %s\n", ptr);
+
+    } else if(bytesRead == 0) {
+
+        buffer_write_adv(buff, 0);
+
+        close(stdinFD);
+        stdinFD = -1;
 
     } else {
 
-        // perror("StdIn read failed");
-        return 0;
-    }
+     perror("StdIn read failed");
 
-    return bytesRead;
+ }
+
 }
 
-static int handleReadPipeFromChild(struct buffer * buff) {
+static void handleWriteStdOut(struct buffer * buff) {
 
-    int bytesRead = 0;
+    ssize_t bytesWritten = 0;
 
-    size_t * bytes = 0;
+    size_t read_bytes[1] = {0};
+
+    uint8_t * ptr = buffer_read_ptr(buff, read_bytes);
+
+    bytesWritten = write(stdoutFD, ptr, *read_bytes);
+
+    if (bytesWritten > 0) {
+
+        buffer_read_adv(buff, bytesWritten);
+
+        printf("Wrote to stdout: %s\n", ptr);
+
+    } else if(bytesWritten == 0) {
+
+        buffer_read_adv(buff, 0);
+
+    } else {
+
+        perror("StdOut write failed");
+
+    }
+
+}
+
+static void handleReadPipeFromChild(struct buffer * buff) {
+
+    size_t bytes[1] = {0};
+
+    ssize_t bytesRead = 0;
+
+    uint8_t * ptr = buffer_write_ptr(buff, bytes);
 
     // Read response from child
-    bytesRead = read(pipeFromChild[0], buffer_write_ptr(buff, bytes), *bytes) ;
+    bytesRead = read(pipeFromChild[0], ptr, *bytes);
 
     if (bytesRead > 0) {
 
-        outParity ^= calcParity(buff);
+        outParity ^= calcParity(ptr, bytesRead);
 
-        fprintf(stderr, "Read from child: %d\n", ((int)*bytes));
+        buffer_write_adv(buff, bytesRead);
+
+        // printf("Read from child: %s\n", ptr);
 
     } else if (bytesRead == 0) {
 
-        return 0;
+        buffer_write_adv(buff, 0);
+
+        close(pipeFromChild[0]);
+        pipeFromChild[0] = -1;
+
     } else {
 
         perror("ReadPipeFromChild read failed");
-        return -1;
     }
 
-    close(pipeFromChild[0]);
-
-    return bytesRead;
 }
 
 static void handleWritePipeToChild(struct buffer * buff) {
 
-    size_t * write_bytes = 0;
+    size_t read_bytes[1];
 
-    if ( write(pipeToChild[1], buffer_read_ptr( buff, write_bytes ), *write_bytes ) > 0 ) {
-        printf("Wrote to child: %d bytes\n", ((int)*write_bytes));
+    uint8_t * ptr = buffer_read_ptr( buff, read_bytes );
+
+    ssize_t writtenBytes = 0;
+
+    if ( (writtenBytes = write(pipeToChild[1], ptr , *read_bytes )) > 0 ) {
+
+        // printf("Wrote to child: %s \n", ptr);
+
+        buffer_read_adv(buff, writtenBytes);
+
+    } else if(writtenBytes == 0) {
+
+        buffer_read_adv(buff, writtenBytes);
+
     }
 
 }
 
-// Source from next 2 functions: http://jhshi.me/2013/11/02/use-select-to-monitor-multiple-file-descriptors/index.html#.Wq8xTmaZNQI
-// add a fd to fd_set, and update max_fd
+// Source from next function: http://jhshi.me/2013/11/02/use-select-to-monitor-multiple-file-descriptors/index.html#.Wq8xTmaZNQI
+// Add a fd to fd_set, and update max_fd
 static int safeFdSet(int fd, fd_set* fds, int* max_fd) {
     assert(max_fd != NULL);
 
@@ -103,53 +156,33 @@ static int safeFdSet(int fd, fd_set* fds, int* max_fd) {
     return 0;
 }
 
-// clear fd from fds, update max fd if needed
-static int safeFdClr(int fd, fd_set* fds, int* max_fd) {
-    assert(max_fd != NULL);
-
-    FD_CLR(fd, fds);
-    if (fd == *max_fd) {
-        (*max_fd)--;
-    }
-    return 0;
-}
-
 static int startParent() {
 
-    // Set up select parameters
-    fd_set readSet;
-    fd_set writeSet;
-    FD_ZERO(&readSet);
-    FD_ZERO(&writeSet);
+    close(pipeToChild[0]);
+    close(pipeFromChild[1]);
 
-    int max_fd = -1;
+    pipeToChild[0] = pipeFromChild[1] = -1;
+
     int n;
 
-
-    //deberiamos anotarnos depende de la condicion, no ?
-   /* safeFdSet(STDIN_FILENO, &readSet, &max_fd);
-    safeFdSet(pipeFromChild[0], &readSet, &max_fd);
-
-    safeFdSet(STDOUT_FILENO, &writeSet, &max_fd);
-    safeFdSet(pipeToChild[1], &writeSet, &max_fd);
-*/
     uint8_t buffin[4096] = {0}, buffout[4096] = {0};
     struct buffer bin, bou;
     buffer_init(&bin, sizeof(buffin)/sizeof(*buffin), buffin);
     buffer_init(&bou, sizeof(buffout)/sizeof(*buffout), buffout);
 
-    fd_set readSetBackup = readSet;
-    fd_set writeSetBackup = writeSet;
-
     do {
         fflush(stdout);
 
-        int bytesRead = 0;
+        int max_fd = 0;
 
+        fd_set readSet;
+        fd_set writeSet;
+        FD_ZERO(&readSet);
+        FD_ZERO(&writeSet);
 
         // Nos inscribimos segun las condiciones
-        if( STDIN_FILENO != -1 && buffer_can_write(&bin) ) {
-            safeFdSet( STDIN_FILENO, &readSet, &max_fd);
+        if( stdinFD != -1 && buffer_can_write(&bin) ) {
+            safeFdSet( stdinFD, &readSet, &max_fd);
         }
 
         if( pipeFromChild[0] != -1 && buffer_can_write(&bou) ) {
@@ -160,63 +193,71 @@ static int startParent() {
             safeFdSet( pipeToChild[1], &writeSet, &max_fd);
         }
 
-        if( STDOUT_FILENO != -1 && buffer_can_read(&bou) ) {
-            safeFdSet( STDOUT_FILENO, &writeSet, &max_fd);
+        if( stdoutFD != -1 && buffer_can_read(&bou) ) {
+            safeFdSet( stdoutFD, &writeSet, &max_fd);
         }
 
 
         n = select(max_fd + 1, &readSet, &writeSet, NULL, NULL);
-        //printf("WTF :\n select: %d\n Pipes: STDIN %d STDOUT %d CHIN %d CHOUT %d \n", n, STDIN_FILENO, STDOUT_FILENO, pipeFromChild[0], pipeToChild[1] );
+        // printf("WTF :\n select: %d\n Pipes: STDIN %d STDOUT %d CHIN %d CHOUT %d \n", n, stdinFD, stdoutFD, pipeFromChild[0], pipeToChild[1] );
 
-        //handlers por si alguno esta disponible
-        if ( FD_ISSET(STDIN_FILENO, &readSet) ) {
+        if ( stdinFD != -1 && FD_ISSET(stdinFD, &readSet) ) {
 
-            // Close unused ends of pipes
-            //close(pipeToChild[0]);
-            //close(pipeFromChild[1]);
+            // printf("%s\n", "stdinFD set");
 
-            bytesRead = handleReadStdIn(&bin);
+            handleReadStdIn(&bin);
         }
 
-        if (FD_ISSET(pipeToChild[1], &writeSet)) {
+        if ( pipeToChild[1] != -1 && FD_ISSET(pipeToChild[1], &writeSet)) {
+
+            // printf("%s\n", "pipeToChild[1] set");
 
             handleWritePipeToChild(&bin);
 
-            close(pipeToChild[1]);
-
         }
 
-        if (FD_ISSET(pipeFromChild[0], &readSet)) {
+        if ( pipeFromChild[0] != -1 && FD_ISSET(pipeFromChild[0], &readSet)) {
+
+            // printf("%s\n", "pipeFromChild[0] set");
 
             handleReadPipeFromChild(&bou);
+
         }
 
-        if (FD_ISSET(STDOUT_FILENO, &writeSet)) {
-            // Is it ok ?
-            fprintf(stderr, "In parity: %#X\nOut parity: %#X\n", inParity, outParity);
+        if ( stdoutFD != -1 && FD_ISSET(stdoutFD, &writeSet)) {
+
+            // printf("%s\n", "stdoutFD set");
+
+            handleWriteStdOut(&bou);
+
         }
 
         // If you can't read, do not write
-        if ( -1 == STDIN_FILENO && -1 != pipeToChild[1] && !buffer_can_read(&bin) ){
+        if ( -1 == stdinFD && -1 != pipeToChild[1] && !buffer_can_read(&bin) ){
             close( pipeToChild[1] );
             pipeToChild[1] = -1;
+
+            // printf("%s\n", "stdinFD & pipeToChild[1] -> CLOSED");
         }
 
-        if ( -1 == pipeFromChild[0] && -1 != STDOUT_FILENO && !buffer_can_read(&bou) ){
-            close( STDOUT_FILENO );
+        if ( -1 == pipeFromChild[0] && -1 != stdoutFD && !buffer_can_read(&bou) ){
+            close( stdoutFD );
+            stdoutFD = -1;
+
+            // fprintf(stderr, "%s\n", "pipeFromChild[0] & stdoutFD -> CLOSED");
         }
 
-        // return if every operation is finished
-        if ( STDIN_FILENO == -1 && pipeFromChild[0] == -1
-            && pipeToChild[1] == -1 && STDOUT_FILENO == -1){
+        // Return if every operation is finished
+        if ( stdinFD == -1 && pipeFromChild[0] == -1 && pipeToChild[1] == -1 && stdoutFD == -1) {
+
+            fprintf(stderr, "In parity: %#X\nOut parity: %#X\n", inParity, outParity);
             return 0;
         }
 
-        readSetBackup = readSet;
-        writeSetBackup = writeSet;
+        // printf("AFTER :\n select: %d\n Pipes: STDIN %d STDOUT %d CHIN %d CHOUT %d \n", n, stdinFD, stdoutFD, pipeFromChild[0], pipeToChild[1] );
 
-    }
-    while ( n != -1 );
+
+    } while ( n != -1 );
 
     wait(NULL);
 
@@ -226,8 +267,15 @@ static int startParent() {
 
 static void startChild(char * command) {
     // Close unused ends of pipes
+    close(stdinFD);
+    close(stdoutFD);
+
+    stdinFD = stdoutFD = -1;
+
     close(pipeToChild[1]);
     close(pipeFromChild[0]);
+
+    pipeToChild[1] = pipeFromChild[0] = -1;
 
     // Duplicate ends of pipes to stdin and stdout
     // Connect child stdin and stdout to parent pipes
