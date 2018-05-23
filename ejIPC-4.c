091efad7a4a469fc00091951b42b870ad354637b
j1nma <j1nma@users.noreply.github.com>
2018-05-23 02:30:07 -0300

    full selector working

--- ejIPC-4.c
+++ ejIPC-4.c
@@ -15,14 +15,12 @@
 
 #define N(x) (sizeof(x) / sizeof((x)[0]))
 
-#define ATTACHMENT(key) ((buffer *)(key)->data)
+#define ATTACHMENT(key) ((struct io_struct *)(key)->data)
 
 static int pipeToChild[2] = {-1, -1};
 static int pipeFromChild[2] = {-1, -1};
 static int stdinFD = STDIN_FILENO;
 static int stdoutFD = STDOUT_FILENO;
-
-static uint8_t inParity = 0x00, outParity = 0x00;
 
 static bool done = false;
 
@@ -33,207 +31,88 @@ sigterm_handler(const int signal)
     done = true;
 }
 
-static uint8_t calcParity(uint8_t *ptr, ssize_t size)
+static void
+parity(const uint8_t *ptr, const ssize_t n, uint8_t *parity)
 {
-    uint8_t parity = 0x00;
-
-    for (ssize_t i = 0; i < size; i++)
+    for (ssize_t i = 0; i < n; i++)
     {
-        parity ^= ptr[i];
+        *parity ^= ptr[i];
     }
-
-    return parity;
 }
 
-static void handleReadStdIn(struct selector_key *key)
+struct io_struct
 {
+    int *fd;
 
-    printf("llegué a handleReadStdIn\n");
+    struct buffer *buff;
 
-    buffer *buff = ATTACHMENT(key);
+    uint8_t *par;
+};
 
-    ssize_t bytesRead = 0;
+int doread(int *fd, struct buffer *buff, uint8_t *par)
+{
+    uint8_t *ptr;
+    ssize_t n;
+    size_t count = 0;
+    int ret = 0;
 
-    size_t write_bytes[1] = {0};
-
-    uint8_t *ptr = buffer_write_ptr(buff, write_bytes);
-
-    bytesRead = read(stdinFD, ptr, *write_bytes);
-
-    if (bytesRead > 0)
+    ptr = buffer_write_ptr(buff, &count);
+    n = read(*fd, ptr, count);
+    if (n == 0 || n == -1)
     {
-
-        inParity ^= calcParity(ptr, bytesRead);
-
-        buffer_write_adv(buff, bytesRead);
-
-        printf("Read from stdin: %s\n", ptr);
+        *fd = -1;
+        ret = -1;
     }
-    else if (bytesRead == 0)
+    else
     {
-
-        buffer_write_adv(buff, 0);
-
-        if (SELECTOR_SUCCESS != selector_unregister_fd(key->s, stdinFD))
+        if (NULL != par)
         {
-            fprintf(stderr, "aborto handleReadStdIn\n");
-            abort();
+            parity(ptr, n, par);
         }
-        close(stdinFD);
-        stdinFD = -1;
+        buffer_write_adv(buff, n);
+    }
 
-        fprintf(stderr, "me fui de handleReadStdIn\n");
+    return ret;
+}
+
+int dowrite(int *fd, struct buffer *buff, uint8_t *par)
+{
+    uint8_t *ptr;
+    ssize_t n;
+    size_t count = 0;
+    int ret = 0;
+
+    ptr = buffer_read_ptr(buff, &count);
+    n = write(*fd, ptr, count);
+    if (n == -1)
+    {
+        *fd = -1;
+        ret = -1;
     }
     else
     {
-        selector_unregister_fd(key->s, stdinFD);
-        close(stdinFD);
-        stdinFD = -1;
-
-        perror("StdIn read failed");
+        if (NULL != par)
+        {
+            parity(ptr, n, par);
+        }
+        buffer_read_adv(buff, n);
     }
+
+    return ret;
 }
 
-static int cut = 0;
-static void handleWriteStdOut(struct selector_key *key)
+static void
+doreadHandler(struct selector_key *key)
 {
-
-    buffer *buff = ATTACHMENT(key);
-
-    ssize_t bytesWritten = 0;
-
-    size_t read_bytes[1] = {0};
-
-    uint8_t *ptr = buffer_read_ptr(buff, read_bytes);
-
-    bytesWritten = write(stdoutFD, ptr, *read_bytes);
-
-    if (bytesWritten > 0)
-    {
-        buffer_read_adv(buff, bytesWritten);
-    }
-    else if (bytesWritten == 0)
-    {
-        buffer_read_adv(buff, 0);
-    }
-    else
-    {
-
-        selector_unregister_fd(key->s, stdoutFD);
-        close(stdoutFD);
-        stdoutFD = -1;
-
-        perror("StdOut write failed");
-    }
-
-    cut += 1;
+    struct io_struct *ios = ATTACHMENT(key);
+    doread(ios->fd, ios->buff, ios->par);
 }
 
-static void handleReadPipeFromChild(struct selector_key *key)
+static void
+doWriteHandler(struct selector_key *key)
 {
-    fprintf(stderr, "llegué a handleReadPipeFromChild\n");
-
-    buffer *buff = ATTACHMENT(key);
-
-    size_t bytes[1] = {0};
-
-    ssize_t bytesRead = 0;
-
-    uint8_t *ptr = buffer_write_ptr(buff, bytes);
-
-    // Read response from child
-    bytesRead = read(pipeFromChild[0], ptr, *bytes);
-
-    if (bytesRead > 0)
-    {
-
-        outParity ^= calcParity(ptr, bytesRead);
-        fprintf(stderr, "handleReadPipeFromChild ptr: %s - bR: %d\n", ptr, bytesRead);
-
-        buffer_write_adv(buff, bytesRead);
-
-        printf("Read from child: %s\n", ptr);
-    }
-    else if (bytesRead == 0)
-    {
-
-        buffer_write_adv(buff, 0);
-
-        selector_unregister_fd(key->s, pipeFromChild[0]);
-        close(pipeFromChild[0]);
-        pipeFromChild[0] = -1;
-
-        fprintf(stderr, "me fui de handleReadPipeFromChild\n");
-    }
-    else
-    {
-        selector_unregister_fd(key->s, pipeFromChild[0]);
-        close(pipeFromChild[0]);
-        pipeFromChild[0] = -1;
-
-        perror("ReadPipeFromChild read failed");
-    }
-}
-
-static void handleWritePipeToChild(struct selector_key *key)
-{
-
-    printf("llegué a handleWritePipeToChild\n");
-
-    buffer *buff = ATTACHMENT(key);
-
-    ssize_t bytesWritten = 0;
-
-    size_t read_bytes[1] = {0};
-
-    uint8_t *ptr = buffer_read_ptr(buff, read_bytes);
-
-    fprintf(stderr, "handleWritePipeToChild ptr: %s - read_bytes: %d \n", ptr, read_bytes[0]);
-
-    bytesWritten = write(pipeToChild[1], ptr, *read_bytes);
-
-    if (bytesWritten > 0)
-    {
-
-        printf("Wrote to child: %s \n", ptr);
-
-        buffer_read_adv(buff, bytesWritten);
-
-        fprintf(stderr, "handleWritePipeToChild bytesWritten: %d\n", bytesWritten);
-        fprintf(stderr, "handleWritePipeToChild buff: %s\n", buff->data);
-    }
-    else if (bytesWritten == 0)
-    {
-
-        buffer_read_adv(buff, bytesWritten);
-
-        fprintf(stderr, "handleWritePipeToChild buff bW=0: %s\n", buff->data);
-
-        // selector_unregister_fd(key->s, pipeToChild[1]);
-        // close(pipeToChild[1]);
-        // pipeToChild[1] = -1;
-
-        fprintf(stderr, "me fui de handleWritePipeToChild\n");
-    }
-    else
-    {
-        buffer_read_adv(buff, bytesWritten);
-        selector_unregister_fd(key->s, pipeToChild[1]);
-        close(pipeToChild[1]);
-        pipeToChild[1] = -1;
-
-        perror("WritePipeToChild read failed");
-    }
-}
-
-static void FDSETWithMax(int fd, fd_set *fds, int *max)
-{
-    FD_SET(fd, fds);
-
-    if (fd > *max)
-    {
-        *max = fd;
-    }
+    struct io_struct *ios = ATTACHMENT(key);
+    dowrite(ios->fd, ios->buff, ios->par);
 }
 
 static int startParent()
@@ -244,14 +123,12 @@ static int startParent()
 
     pipeToChild[0] = pipeFromChild[1] = -1;
 
-    int n;
+    uint8_t inParity = 0x00, outParity = 0x00;
 
     uint8_t buffin[4096], buffout[4096];
     buffer bin, bou;
     buffer_init(&bin, N(buffin), buffin);
     buffer_init(&bou, N(buffout), buffout);
-
-    // const char *error_msg;
 
     fd_selector selector;
 
@@ -283,25 +160,26 @@ static int startParent()
 
     /* Set fd handlers. */
     const struct fd_handler read_stdin_handler = {
-        .handle_read = &handleReadStdIn,
+        .handle_read = &doreadHandler,
         .handle_write = NULL,
         .handle_close = NULL};
 
     const struct fd_handler write_pipe_to_child_handler = {
         .handle_read = NULL,
-        .handle_write = &handleWritePipeToChild,
+        .handle_write = &doWriteHandler,
         .handle_close = NULL};
 
     const struct fd_handler read_pipe_from_child_handler = {
-        .handle_read = &handleReadPipeFromChild,
+        .handle_read = &doreadHandler,
         .handle_write = NULL,
         .handle_close = NULL};
 
     const struct fd_handler write_stdout_handler = {
         .handle_read = NULL,
-        .handle_write = &handleWriteStdOut,
+        .handle_write = &doWriteHandler,
         .handle_close = NULL};
 
+    /* Set fd to non-blocking. */
     if (selector_fd_set_nio(stdinFD) == -1)
     {
         close(stdinFD);
@@ -334,15 +212,43 @@ static int startParent()
         return 1;
     }
 
+    struct io_struct *ios_stdin;
+    ios_stdin = malloc(sizeof(*ios_stdin));
+    memset(ios_stdin, 0x00, sizeof(*ios_stdin));
+    ios_stdin->fd = &stdinFD;
+    ios_stdin->buff = &bin;
+    ios_stdin->par = &inParity;
+
+    struct io_struct *ios_read_from_child;
+    ios_read_from_child = malloc(sizeof(*ios_read_from_child));
+    memset(ios_read_from_child, 0x00, sizeof(*ios_read_from_child));
+    ios_read_from_child->fd = &pipeFromChild[0];
+    ios_read_from_child->buff = &bou;
+    ios_read_from_child->par = &outParity;
+
+    struct io_struct *ios_write_to_child;
+    ios_write_to_child = malloc(sizeof(*ios_write_to_child));
+    memset(ios_write_to_child, 0x00, sizeof(*ios_write_to_child));
+    ios_write_to_child->fd = &pipeToChild[1];
+    ios_write_to_child->buff = &bin;
+    ios_write_to_child->par = NULL;
+
+    struct io_struct *ios_stdout;
+    ios_stdout = malloc(sizeof(*ios_stdout));
+    memset(ios_stdout, 0x00, sizeof(*ios_stdout));
+    ios_stdout->fd = &stdoutFD;
+    ios_stdout->buff = &bou;
+    ios_stdout->par = NULL;
+
     /* Register fds to selector. */
     selector_status ss_read_stdin = selector_register(selector, stdinFD,
-                                                      &read_stdin_handler, OP_READ, &bin);
+                                                      &read_stdin_handler, OP_READ, ios_stdin);
     selector_status ss_write_pipe_to_child = selector_register(selector, pipeToChild[1],
-                                                               &write_pipe_to_child_handler, OP_WRITE, &bin);
+                                                               &write_pipe_to_child_handler, OP_WRITE, ios_write_to_child);
     selector_status ss_read_pipe_from_child = selector_register(selector, pipeFromChild[0],
-                                                                &read_pipe_from_child_handler, OP_READ, &bou);
+                                                                &read_pipe_from_child_handler, OP_READ, ios_read_from_child);
     selector_status ss_write_stdout = selector_register(selector, stdoutFD,
-                                                        &write_stdout_handler, OP_WRITE, &bou);
+                                                        &write_stdout_handler, OP_WRITE, ios_stdout);
 
     // registrar sigterm es útil para terminar el programa normalmente.
     // esto ayuda mucho en herramientas como valgrind.
@@ -415,101 +321,6 @@ static int startParent()
 
     selector_destroy(selector);
     selector_close();
-
-    // do
-    // {
-
-    //     int max_fd = 0;
-
-    //     fd_set readSet;
-    //     fd_set writeSet;
-    //     FD_ZERO(&readSet);
-    //     FD_ZERO(&writeSet);
-
-    //     // Nos inscribimos segun las condiciones
-    //     if (stdinFD != -1 && buffer_can_write(&bin))
-    //     {
-    //         FDSETWithMax(stdinFD, &readSet, &max_fd);
-    //     }
-
-    //     if (pipeFromChild[0] != -1 && buffer_can_write(&bou))
-    //     {
-    //         FDSETWithMax(pipeFromChild[0], &readSet, &max_fd);
-    //     }
-
-    //     if (pipeToChild[1] != -1 && buffer_can_read(&bin))
-    //     {
-    //         FDSETWithMax(pipeToChild[1], &writeSet, &max_fd);
-    //     }
-
-    //     if (stdoutFD != -1 && buffer_can_read(&bou))
-    //     {
-    //         FDSETWithMax(stdoutFD, &writeSet, &max_fd);
-    //     }
-
-    //     n = select(max_fd + 1, &readSet, &writeSet, NULL, NULL);
-    //     // printf("WTF :\n select: %d\n Pipes: STDIN %d STDOUT %d CHIN %d CHOUT %d \n", n, stdinFD, stdoutFD, pipeFromChild[0], pipeToChild[1] );
-
-    //     if (stdinFD != -1 && FD_ISSET(stdinFD, &readSet))
-    //     {
-
-    //         // printf("%s\n", "stdinFD set");
-
-    //         handleReadStdIn(&bin);
-    //     }
-
-    //     if (pipeToChild[1] != -1 && FD_ISSET(pipeToChild[1], &writeSet))
-    //     {
-
-    //         // printf("%s\n", "pipeToChild[1] set");
-
-    //         handleWritePipeToChild(&bin);
-    //     }
-
-    //     if (pipeFromChild[0] != -1 && FD_ISSET(pipeFromChild[0], &readSet))
-    //     {
-
-    //         // printf("%s\n", "pipeFromChild[0] set");
-
-    //         handleReadPipeFromChild(&bou);
-    //     }
-
-    //     if (stdoutFD != -1 && FD_ISSET(stdoutFD, &writeSet))
-    //     {
-
-    //         // printf("%s\n", "stdoutFD set");
-
-    //         handleWriteStdOut(&bou);
-    //     }
-
-    //     // If you can't read, do not write
-    //     if (-1 == stdinFD && -1 != pipeToChild[1] && !buffer_can_read(&bin))
-    //     {
-    //         close(pipeToChild[1]);
-    //         pipeToChild[1] = -1;
-
-    //         // printf("%s\n", "stdinFD & pipeToChild[1] -> CLOSED");
-    //     }
-
-    //     if (-1 == pipeFromChild[0] && -1 != stdoutFD && !buffer_can_read(&bou))
-    //     {
-    //         close(stdoutFD);
-    //         stdoutFD = -1;
-
-    //         // fprintf(stderr, "%s\n", "pipeFromChild[0] & stdoutFD -> CLOSED");
-    //     }
-
-    //     // Return if every operation is finished
-    //     if (stdinFD == -1 && pipeFromChild[0] == -1 && pipeToChild[1] == -1 && stdoutFD == -1)
-    //     {
-
-    //         fprintf(stderr, "In parity: %#X\nOut parity: %#X\n", inParity, outParity);
-    //         return 0;
-    //     }
-
-    //     // printf("AFTER :\n select: %d\n Pipes: STDIN %d STDOUT %d CHIN %d CHOUT %d \n", n, stdinFD, stdoutFD, pipeFromChild[0], pipeToChild[1] );
-
-    // } while (n != -1);
 
     wait(NULL);
 
